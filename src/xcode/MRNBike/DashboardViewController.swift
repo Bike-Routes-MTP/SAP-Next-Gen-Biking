b9086c9a6f0dc2b2711d36ff7d0d5cb03620d1ea
Ziad Abdelkader <zabdelka@mail.uni-mannheim.de>
2017-05-02 11:28:40 +0200

    - Add Tab Bar Library - Created Screen - Added items - Added Animations - Divide Dashboard Screen - Add images - Add Navigation

---
+++ src/xcode/MRNBike/DashboardViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DashboardViewController.swift
+//  MRNBike
+//
+//  Created by Ziad Abdelkader on 5/1/17.
+//  Copyright © 2017 Marc Bormeth. All rights reserved.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    @IBOutlet weak var batatesView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+       
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
